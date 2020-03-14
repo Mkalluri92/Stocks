5747a8d25ef71b428f8115b85cee233f34c5fc42
mkalluri92 <mkalluri92@gmail.com>
2020-03-14 02:42:43 -0700

    added search component

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
     return (
       <React.Fragment>
         <Header></Header>
-
+        <Search></Search>
        
       </React.Fragment>
     )
--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,15 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 import classes from './Search.module.css';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { FaSearch } from 'react-icons/fa';
 
 
-class Search extends Component {
-    render () {
-        return (
-            <input className={classes.search} type="text" 
-            placeholder="Enter the Company name or Stock sign"></input>
-        )
-        
-    }
+const Search = () => {
+    return (
+        <div className={classes.search}>
+            <input className={classes.searchInput} type="text" 
+            placeholder="Enter the Company name or Stock sign">
+            </input>
+           <FaSearch className={classes.searchIcon}/>
+        </div>
+       
+    )
 }
 
 export default Search;
