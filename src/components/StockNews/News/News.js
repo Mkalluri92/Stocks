989bd0023e12764228afb0f67f8664cf258e8745
mkalluri92 <mkalluri92@gmail.com>
2020-03-17 23:16:19 -0700

    fixed news link

--- src/components/StockNews/News/News.js
+++ src/components/StockNews/News/News.js
@@ -4,23 +4,24 @@ import classes from './News.module.css'
 const News = (props) => {
 
     return(
-        <div className= {classes.headlines}>
-            <img className= {classes.image} 
-                src={props.urlToImage} alt="headline">
-            </img>
-            <div className={classes.details}>
-                <a className={classes.title} 
-                     href={props.url} target="_blank">
-                    {props.title}
-                </a>
-                <div className={classes.content} 
-                    dangerouslySetInnerHTML={{__html: props.content != null ?
-                        props.content.replace(/\[\+[0-9]+.* chars\]/g, "") : 
-                        null}}>
-                    
+        <a href={props.url} target="_blank">
+            <div className= {classes.headlines}>
+                <img className= {classes.image} 
+                    src={props.urlToImage} alt="headline">
+                </img>
+                <div className={classes.details}>
+                    <a className={classes.title} 
+                        href={props.url} target="_blank">
+                        {props.title}
+                    </a>
+                    <div className={classes.content} 
+                        dangerouslySetInnerHTML={{__html: props.content != null ?
+                            props.content.replace(/\[\+[0-9]+.* chars\]/g, "") : 
+                            null}}>
+                    </div>
                 </div>
             </div>
-        </div>
+        </a>
     )
 }
 
